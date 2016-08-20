cd5b38195435e63d973c9a2090adf4e3df8d8ddc
Jeanne Sneed <jsneed@wsgc.com>
2016-08-19 18:16:10 -0700

    http://guides.rubyonrails.org/getting_started.html - through section 5.9

--- Lecture-006/blog/app/controllers/articles_controller.rb
+++ Lecture-006/blog/app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
     #order: index, show, new, edit, create, update and destroy
 
+    def index
+        @articles = Article.all
+    end
+
     def show
         @article = Article.find(params[:id])
     end
