363c52d80a93e6e7d31fd19b6a96d845d4a0f920
Jeanne Sneed <jsneed@wsgc.com>
2016-08-19 17:36:49 -0700

    http://guides.rubyonrails.org/getting_started.html - through section 5.7

--- Lecture-006/blog/config/routes.rb
+++ Lecture-006/blog/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles
+  
+  # Tells the server which route to load at startup
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
