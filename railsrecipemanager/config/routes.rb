c88821fe60626d5feffe8decfefe08b1fdcbd3ba
Jeanne Sneed <jsneed@wsgc.com>
2016-08-22 17:51:32 -0700

    Through Lecture 14 - Add Bootstrap and Home Page Elements

--- railsrecipemanager/config/routes.rb
+++ railsrecipemanager/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/home', to: 'pages#home'
 
+  #get 'pages/home'
+
   get 'welcome/index'
 
   resources :articles do
@@ -8,7 +10,8 @@ Rails.application.routes.draw do
   end
 
   # Tells the server which route to load at startup
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
