b154d89af8083df215325264fe1aaa68ac2a1bb2
Jeanne Sneed <jsneed@gmail.com>
2016-08-25 21:27:59 -0700

    Through Lecture 31 - List & Show Recipes

--- RecipeManager/config/routes.rb
+++ RecipeManager/config/routes.rb
@@ -1,17 +1,27 @@
 Rails.application.routes.draw do
-  get '/home', to: 'pages#home'
+    get '/home', to: 'pages#home'
 
-  #get 'pages/home'
+    resources :recipes
+    #All of these route listings can be replaced by the line above
+    #get       '/recipes',             to: 'recipes#index'
+    #get       '/recipes/new',         to: 'recipes#new',      as: 'new_recipe'
+    #post      '/recipes',             to: 'recipes#create'
+    #get       '/recipes/:id/edit',    to: 'recipes#edit',     as: 'edit_recipe'
+    #patch     '/recipes/:id',         to: 'recipes#update'
+    #get       '/recipes/:id',         to: 'recipes#show',     as: 'recipe'
+    #delete    'recipes/:id',          to: 'recipes#destroy'
 
-  get 'welcome/index'
+    #get 'pages/home'
 
-  resources :articles do
-      resources :comments
-  end
+    get 'welcome/index'
 
-  # Tells the server which route to load at startup
-  #root 'welcome#index'
-  root 'pages#home'
+    resources :articles do
+        resources :comments
+    end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # Tells the server which route to load at startup
+    #root 'welcome#index'
+    root 'pages#home'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
