74edf0a568cb80f451f007df83bec0e35fd78213
Jeanne Sneed <jsneed@gmail.com>
2016-08-30 21:33:38 -0700

    Through Lecture 38 - Add Partials & Pictures

--- RecipeManager/db/schema.rb
+++ RecipeManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825044250) do
+ActiveRecord::Schema.define(version: 20160831034335) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160825044250) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
